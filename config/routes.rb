0d64d2b33079c50d39dbbbf9c3717b8579148bf1
Stacy Andrews <stacy.andrews@iinet.net.au>
2014-07-26 11:53:19 +0800

    initial bootstrap integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'spa/index'
+
+  root 'spa#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
