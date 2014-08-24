aa29ce1fd3677b37e89404089fa608ac2b290931
Stacy Andrews <stacy.andrews@iinet.net.au>
2014-08-24 12:35:25 +0800

    clean up

--- app/controllers/catalogue_foods_controller.rb
+++ app/controllers/catalogue_foods_controller.rb
@@ -1,28 +1,20 @@
 class CatalogueFoodsController < ApplicationController
   before_action :set_catalogue_food, only: [:show, :edit, :update, :destroy]
 
-  # GET /catalogue_foods
-  # GET /catalogue_foods.json
   def index
     @catalogue_foods = CatalogueFood.all
   end
 
-  # GET /catalogue_foods/1
-  # GET /catalogue_foods/1.json
   def show
   end
 
-  # GET /catalogue_foods/new
   def new
     @catalogue_food = CatalogueFood.new
   end
 
-  # GET /catalogue_foods/1/edit
   def edit
   end
 
-  # POST /catalogue_foods
-  # POST /catalogue_foods.json
   def create
     @catalogue_food = CatalogueFood.new(catalogue_food_params)
 
@@ -37,8 +29,6 @@ class CatalogueFoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /catalogue_foods/1
-  # PATCH/PUT /catalogue_foods/1.json
   def update
     respond_to do |format|
       if @catalogue_food.update(catalogue_food_params)
@@ -51,8 +41,6 @@ class CatalogueFoodsController < ApplicationController
     end
   end
 
-  # DELETE /catalogue_foods/1
-  # DELETE /catalogue_foods/1.json
   def destroy
     @catalogue_food.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class CatalogueFoodsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_catalogue_food
       @catalogue_food = CatalogueFood.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def catalogue_food_params
       params.require(:catalogue_food).permit(:description, :unit_energy, :carbohydrate, :protein, :fat)
     end
